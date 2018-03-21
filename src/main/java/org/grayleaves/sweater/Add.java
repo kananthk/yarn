817fef9e526290031cc818794e594235855ea2cd
kananthk <31537650+kananthk@users.noreply.github.com>
2018-03-21 11:47:31 -0700

    adding add

---
+++ src/main/java/org/grayleaves/sweater/Add.java
@@ -0,0 +1,24 @@
+package org.grayleaves.sweater;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/v1/color")
+public class Color {
+
+	@GET
+	@Path("{color}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ControlResponse knit(@PathParam("color") String color) {
+		ControlResponse response = new ControlResponse(); 
+		response.setColor(color);
+		return  response; 
+
+	}
+
+
+}
