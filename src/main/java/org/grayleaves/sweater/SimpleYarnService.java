d7870dcf846a02a451fc0618daf4f6b9544b931a
kananthk <31537650+kananthk@users.noreply.github.com>
2018-03-21 13:42:54 -0700

    for yarn

--- src/main/java/org/grayleaves/sweater/SimpleYarnService.java
+++ src/main/java/org/grayleaves/sweater/SimpleYarnService.java
@@ -5,23 +5,41 @@ import java.util.Map;
 
 
 
-public class SimpleYarnService implements YarnService {
+public class SimpleYarnStash implements YarnStash {
 
 	private Map<YarnEnum, Integer> yarn = new HashMap<>(); 
-	public SimpleYarnService(YarnEnum color, int yards) {
+	public SimpleYarnStash(YarnEnum color, int yards) {
 		yarn.put(color, yards);
 	}
 
+	public SimpleYarnStash() {
+	}
+
 	@Override
-	public int getYarn(YarnEnum color, int yards) {
+	public int use(YarnEnum color, int yards) {
 		int available = 0; 
 		if (yarn.containsKey(color)) {
 			available = yarn.get(color);
 			if (available > yards) {
+				yarn.put(color, (available - yards)); 
 				available = yards; 
+			}
+			else {
+				yarn.put(color, 0); 
 			}
 		}
 		return available;
 	}
+
+	@Override
+	public int add(YarnEnum color, int yards) {
+		Integer available = yarn.get(color);
+		if (available != null) {
+			yards = yards + available; 
+		}
+		yarn.put(color, yards); 
+		return yards; 
+	}
+
 
 }
