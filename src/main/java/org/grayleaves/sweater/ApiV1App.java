3e49cf046b62ca8898abb6bf9133990f70fc4418
kananthk <31537650+kananthk@users.noreply.github.com>
2018-03-21 13:40:00 -0700

    for yarn

--- src/main/java/org/grayleaves/sweater/ApiV1App.java
+++ src/main/java/org/grayleaves/sweater/ApiV1App.java
@@ -4,26 +4,18 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
-
 	@ApplicationPath("/api/*")
 	public class ApiV1App extends Application {
 	    public ApiV1App() {
-	    	System.out.println("liberty reached");
+	    	System.out.println("yarn reached");
 	    	configure();
 	    }
-	    public static void configure() {
-	    	determineInstance();
-	    	String flag = System.getenv("USE_WEB_YARN_SERVICE");
-	    	System.out.println("flag: "+flag);
-	    	if ((flag != null) && (flag.equalsIgnoreCase("true"))) {
-	    		KnitStatusResponse.setYarnService(new WebYarnService()); 
-	    		System.out.println("configuring WebYarnService");
-	    	} else {
-	    		KnitStatusResponse.setYarnService(new SimpleYarnService(YarnEnum.GRAY, 10)); 
-	    	}
-	    }
-		public static void determineInstance() {
-			HealthResponse.INSTANCE_VALUE = System.getenv(HealthResponse.INSTANCE_KEY);
-		}
 	    
+    public static void configure() {
+    	determineInstance();
+    }
+	public static void determineInstance() {
+		HealthResponse.INSTANCE_VALUE = System.getenv(HealthResponse.INSTANCE_KEY);
+	}
+
 }
